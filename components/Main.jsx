ef312f0e2e665a7560624927d505801b08280b91
Mikhael Edman Gomez <edman.gomez.business@gmail.com>
2025-05-06 13:05:52 +0800

    first commit

---
+++ components/Main.jsx
@@ -0,0 +1,46 @@
+import React from "react"
+import IngredientsList from "/components/main_components/IngredientsList.jsx"
+import ClaudeRecipe from "/components/main_components/ClaudeRecipe"
+import { getRecipeFromMistral } from "/ai"
+
+export default function Main() {
+    const [ingredients, setIngredients] = React.useState([])
+    const [recipe, setRecipe] = React.useState("")
+
+    async function getRecipe() {
+        const recipeMarkdown = await getRecipeFromMistral(ingredients)
+        setRecipe(recipeMarkdown)
+    }
+
+    function addIngredient(event) {
+        event.preventDefault()
+        const form = event.target; // form element
+        const formData = new FormData(form);
+        const newIngredient = formData.get("ingredient")
+        setIngredients(prevIngredients => [...prevIngredients, newIngredient])
+        form.reset();
+    }
+
+    return (
+        <main>
+            <form onSubmit={addIngredient} className="add-ingredient-form">
+                <input
+                    type="text"
+                    placeholder="e.g. oregano"
+                    aria-label="Add ingredient"
+                    name="ingredient"
+                />
+                <button>Add ingredient</button>
+            </form>
+
+            {ingredients.length > 0 &&
+                <IngredientsList
+                    ingredients={ingredients}
+                    getRecipe={getRecipe}
+                />
+            }
+
+            {recipe && <ClaudeRecipe recipe={recipe} />}
+        </main>
+    )
+}
